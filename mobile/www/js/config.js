0f2d0fa6e416cd7c03e3885db5be5f8b94bedb8f
Sameer Vedpathak <sameer.vedpathak18@gmail.com>
2017-03-06 15:07:45 +0530

    config changes

--- mobile/www/js/config.js
+++ mobile/www/js/config.js
@@ -1,1 +1,7 @@
-var baseURL = 'http://localhost:2000/api/';
+var env = 'local';
+
+if(env === 'dev'){
+  var baseURL = 'http://localhost:5000/api/';
+}else if(env === 'dev'){
+  var baseURL= "http://node.fountaintechies.com:5000/api/";
+}
--- todoapp.js
+++ todoapp.js
@@ -13,10 +13,9 @@ var TodoCRUD = CRUD(connection, 'student');
 
 var app = express();
 
+var web = connect();
 var mobile = connect();
 
-mobile.use(serveStatic('mobile/www'));
-app.use('/mobile',mobile);
 
 // handle cors
 app.use(function(req, res, next) {
@@ -26,6 +25,13 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', false);
   next();
 });
+
+web.use(serveStatic('web'));
+app.use('/',web);
+
+mobile.use(serveStatic('mobile/www'));
+app.use('/mobile',mobile);
+
 
 app.use(bodyParser.json({ limit: '50mb', extended: true, type:'application/json' }));
 app.use(bodyParser.json({ type: 'application/*+json' }));
@@ -42,5 +48,5 @@ app.get('/api/getallstudentlist' , function(req,res){
 	})
 });
 
-app.listen(2000);
-console.log("server running on port 2000");
+app.listen(5000);
+console.log("server running on port 5000");
