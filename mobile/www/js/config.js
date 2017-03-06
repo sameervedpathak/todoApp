8b6e091885d6362762ad2ee97bd96f543c9883f3
Sameer Vedpathak <sameer.vedpathak18@gmail.com>
2017-03-06 15:17:23 +0530

    config changes

--- mobile/www/js/config.js
+++ mobile/www/js/config.js
@@ -1,6 +1,6 @@
-var env = 'local';
+var env = 'dev';
 
-if(env === 'dev'){
+if(env === 'local'){
   var baseURL = 'http://localhost:5000/api/';
 }else if(env === 'dev'){
   var baseURL= "http://node.fountaintechies.com:5000/api/";
